9601345c9ec4faf3d884f324e864dc9b79ef5229
Brian Michel Ibañez <98329948+chaeBrian@users.noreply.github.com>
2023-03-28 15:02:12 -0300

    first commit

---
+++ 00_hello_world.py
@@ -0,0 +1,15 @@
+# Hola mundo
+print("Hola Mundo")
+
+"""
+Este es un comentario 
+en varias lineas
+"""
+
+# Consultar el tipo de dato
+
+print(type("Hola Mundo")) # Tipo 'str' / string
+print(type(4)) # Tipo 'int' / enteros
+print(type(4.5)) # Tipo 'float' / decimales
+print(type(4 - 1j)) # Tipo 'complex' / complejos
+print(type(True)) # Tipo 'bool' / booleano
