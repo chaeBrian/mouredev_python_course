b63bfc6abd72a16e58ce22501b9469e0866b80a3
chaeBrian <bmichelibanez@gmail.com>
2023-04-05 23:32:43 -0300

    dictionaries 2/2

--- 07_dicts.py
+++ 07_dicts.py
@@ -27,5 +27,47 @@ print(len(my_dict))
 
 # Busqueda
 
-print(my_dict[1])
+# 'print(my_dict)'
 print(my_dict["Name"])
+print("Messi" in my_dict)
+print("Username" in my_dict)
+
+# Inserción
+
+my_dict["S-Number"] = 10
+print(my_dict)
+
+# Actualización
+
+my_dict["Name"] = "Lionel Andres"
+print(my_dict["Name"])
+
+# Eliminación
+
+del my_dict["S-Number"]
+print(my_dict)
+
+# Otras operaciones
+
+print(my_dict.items())
+print(my_dict.keys())
+print(my_dict.values())
+
+my_list = ["Name", 10, "Nationality"]
+
+my_new_dict = dict.fromkeys((my_list))
+print(my_new_dict)
+my_new_dict = dict.fromkeys(("Name", 10, "Nationality"))
+print((my_new_dict))
+my_new_dict = dict.fromkeys(my_dict)
+print((my_new_dict))
+my_new_dict = dict.fromkeys(my_dict, "PSG")
+print((my_new_dict))
+
+my_values = my_new_dict.values()
+print(type(my_values))
+
+print(my_new_dict.values())
+print(list(dict.fromkeys(list(my_new_dict.values())).keys()))
+print(tuple(my_new_dict))
+print(set(my_new_dict))
